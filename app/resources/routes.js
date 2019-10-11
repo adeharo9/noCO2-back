ab53abf83fe700baf3c11ec6a86b10cbc77da3ed
Alejandro de Haro <a.deharo9@gmail.com>
2019-10-12 00:40:51 +0200

    First files

---
+++ app/resources/routes.js
@@ -0,0 +1,12 @@
+// Initialize express router
+const router = require("express").Router();
+
+// Load endpoints
+const root       = require("./endpoints/root");
+const emissions  = require("./endpoints/emissions");
+
+root      (router);
+emissions (router);
+
+// Export API routes
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-/* noCO2 project */
+// noCO2 project
+// Dependencies
+require("dotenv").config({ silent: true });
+
+const cors       = require("cors");
+const http       = require("http");
+const express    = require("express");
+const bodyParser = require("body-parser");
+
+// Initialize and configure app
+const app = express();
+
+app.use(cors({origin: '*'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Initialize endpoints
+const routes = require("./app/resources/routes");
+app.use('/', routes);
+
+// Start server
+let serverHttp;
+
+if (process.env.ENV == "prd")
+{
+    serverHttp = http.createServer(app).listen(process.env.PORT_HTTP_PRD || 80, () =>
+    {
+        console.log("Scheduling API running on port", serverHttp.address().port);
+    });
+}
+else
+{
+    serverHttp = http.createServer(app).listen(process.env.PORT_HTTP_DEV || 8080, () =>
+    {
+        console.log("Scheduling API running on port", serverHttp.address().port);
+    });
+}
