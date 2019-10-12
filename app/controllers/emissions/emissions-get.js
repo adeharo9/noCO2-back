8d068bb31548a58cb5da16bbd7109166ac6b01df
Alejandro de Haro <a.deharo9@gmail.com>
2019-10-12 04:29:01 +0200

    ASDF

--- app/controllers/emissions/emissions-get.js
+++ app/controllers/emissions/emissions-get.js
@@ -1,18 +1,41 @@
-const request = require('request');
+const request = require('request-promise');
+const child_process = require('child_process');
 const HttpStatusCode  = require('../../resources/http-status-code');
 
 const baseURL = 'https://maps.googleapis.com/maps/api/directions/json';
 
 module.exports = (query) =>
 {
-    return new Promise((resolve, reject) =>
+    const options = {
+        uri: process.env.ROUTE_PROVIDER_BASE_URL,
+        qs: query,
+        json: true
+    };
+
+    return request(options).then((res) =>
     {
-        // Requestear a API GMaps
-        // Llamar C++
-        // Apendear respuesta C++ a respuesta GMaps
+        let coreOutput;
+        child_process.execSync(process.env.CORE_PROCESS_PATH,
+        {
+            stdio: [JSON.stringify(res), process.stdout, process.stderr]
+        });
+        
+        const response = coreOutput;
+        const success = HttpStatusCode.ok(response['status']);
 
-        const response = HttpStatusCode.ok('success');
+        for (const key in success)
+        {
+            if (success.hasOwnProperty(key))
+            {
+                response[key] = success[key];
+            }
+        }
 
-        return resolve(response);
+        return response;
+    })
+    .catch((err) =>
+    {
+        const response = HttpStatusCode.internalServerError('error while requesting route to route provider');
+        return response;
     });
 };
